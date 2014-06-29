9afe32c3c1c56437587ff2c97caed6cb8b57a1fa
Moritz Schulze <moritz.schulze@gmail.com>
2014-06-29 18:01:10 +0200

    Added sql files with some initial data for both the oauth and resource database.

---
+++ sql/resource_data.sql
@@ -0,0 +1,11 @@
+INSERT INTO "authority" VALUES(0,'ROLE_OAUTH_ADMIN');
+INSERT INTO "authority" VALUES(1,'ROLE_ADMIN');
+INSERT INTO "authority" VALUES(2,'ROLE_USER');
+INSERT INTO "credentials" VALUES(0,1,'oauth_admin','admin',0);
+INSERT INTO "credentials" VALUES(1,1,'resource_admin','admin',0);
+INSERT INTO "credentials" VALUES(2,1,'user','user',0);
+INSERT INTO "credentials_authorities" VALUES(0,0);
+INSERT INTO "credentials_authorities" VALUES(1,1);
+INSERT INTO "credentials_authorities" VALUES(2,2);
+INSERT INTO "todo" (id, done, done_time, message, version) VALUES (1, 0, null, 'Write an oauth example application.', 0);
+INSERT INTO "todo" (id, done, done_time, message, version) VALUES (2, 1, '1403947411000', 'Do grocery shopping.', 0);
