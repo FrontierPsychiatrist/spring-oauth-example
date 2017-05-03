a98963950ba631819d714217b61b54093e19c86a
moritz.schulze <moritz.schulze@gmail.com>
2017-05-03 17:18:00 +0200

    Upgrades to Spring Boot 1.5.3
    Also adds http://localhost:9090 to the allowed return_to URIs since
    that is used in the README.md.

--- oauth-server/src/main/resources/db/migration/V0__initial_schema_and_data.sql
+++ oauth-server/src/main/resources/db/migration/V0__initial_schema_and_data.sql
@@ -52,7 +52,7 @@ create table oauth_approvals (
 INSERT INTO oauth_client_details
   (client_id, resource_ids, client_secret, scope, authorized_grant_types, web_server_redirect_uri, authorities, access_token_validity, refresh_token_validity, additional_information, autoapprove)
 VALUES
-  ('read-only-client', 'todo-services', null, 'read', 'implicit', 'http://localhost', NULL, 7200, 0, NULL, 'false');
+  ('read-only-client', 'todo-services', null, 'read', 'implicit', 'http://localhost,http://localhost:9090', NULL, 7200, 0, NULL, 'false');
 
 INSERT INTO oauth_client_details
   (client_id, resource_ids, client_secret, scope, authorized_grant_types, web_server_redirect_uri, authorities, access_token_validity, refresh_token_validity, additional_information, autoapprove)
